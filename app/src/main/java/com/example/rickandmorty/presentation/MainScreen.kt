77b64a94590f599c79e784f6ab905e68a56e7bc9
Lobiofrom <124072945+Lobiofrom@users.noreply.github.com>
2023-12-12 10:56:40 +0300

    n

--- app/src/main/java/com/example/rickandmorty/presentation/MainScreen.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/MainScreen.kt
@@ -1,7 +1,6 @@
 package com.example.rickandmorty.presentation
 
 import android.annotation.SuppressLint
-import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -33,13 +32,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.rememberNavController
 import com.example.rickandmorty.navigation.BottomNaviItem
 import com.example.rickandmorty.navigation.Navigation
-import com.example.rickandmorty.ui.theme.RickandMortyTheme
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
