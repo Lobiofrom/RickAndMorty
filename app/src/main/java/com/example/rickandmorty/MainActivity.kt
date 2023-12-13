0519983721b0dc488c55d290177361b660c2ba40
Lobiofrom <124072945+Lobiofrom@users.noreply.github.com>
2023-12-13 21:11:53 +0300

    n

--- app/src/main/java/com/example/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/example/rickandmorty/MainActivity.kt
@@ -7,25 +7,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.rickandmorty.di.appModule
 import com.example.rickandmorty.presentation.MainScreen
 import com.example.rickandmorty.ui.theme.RickandMortyTheme
-import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
-import org.koin.core.context.GlobalContext.startKoin
 
 class MainActivity : ComponentActivity() {
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (savedInstanceState == null) {
-            startKoin {
-                androidLogger()
-                androidContext(this@MainActivity)
-                modules(appModule)
-            }
-        }
         setContent {
             RickandMortyTheme {
                 Surface(
